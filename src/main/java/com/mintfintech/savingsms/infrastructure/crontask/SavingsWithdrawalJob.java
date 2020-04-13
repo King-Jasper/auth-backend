1f564ce68dde418656cc771f30d7a2424c6b535b
jnwanya <nwanyajustin@gmail.com>
2020-04-13 20:05:13 +0100

    Added log to fund withdrawal cron expression

--- src/main/java/com/mintfintech/savingsms/infrastructure/crontask/SavingsWithdrawalJob.java
+++ src/main/java/com/mintfintech/savingsms/infrastructure/crontask/SavingsWithdrawalJob.java
@@ -1,6 +1,7 @@
 package com.mintfintech.savingsms.infrastructure.crontask;
 
 import com.mintfintech.savingsms.usecase.FundWithdrawalUseCase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.inject.Named;
@@ -9,6 +10,7 @@ import javax.inject.Named;
  * Created by jnwanya on
  * Tue, 07 Apr, 2020
  */
+@Slf4j
 @Named
 public class SavingsWithdrawalJob {
 
@@ -17,13 +19,12 @@ public class SavingsWithdrawalJob {
         this.fundWithdrawalUseCase = fundWithdrawalUseCase;
     }
 
-    @Scheduled(cron = "0 0/5 0 ? * *") // runs by every 5 minutes
+    @Scheduled(cron = "0 0/5 * ? * *") // runs by every 5 minutes
     public void processInterestAccountCrediting(){
-
         fundWithdrawalUseCase.processInterestCreditForFundWithdrawal();
     }
 
-    @Scheduled(cron = "0 0/5 0 ? * *") // runs by every 5 minutes
+    @Scheduled(cron = "0 0/5 * ? * *") // runs by every 5 minutes
     public void processSavingAccountFunding(){
        fundWithdrawalUseCase.processSavingFundCrediting();
     }
