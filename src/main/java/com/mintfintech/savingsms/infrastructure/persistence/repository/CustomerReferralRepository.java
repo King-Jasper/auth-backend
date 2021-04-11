de10b3b3073ab2839ccec8bc5dfcc51538939c4f
jnwanya <nwanyajustin@gmail.com>
2021-04-11 11:29:07 +0100

    updated query for referral processing.

--- src/main/java/com/mintfintech/savingsms/infrastructure/persistence/repository/CustomerReferralRepository.java
+++ src/main/java/com/mintfintech/savingsms/infrastructure/persistence/repository/CustomerReferralRepository.java
@@ -33,6 +33,6 @@ public interface CustomerReferralRepository extends JpaRepository<CustomerReferr
     @Query("select c from CustomerReferralEntity c where c.dateCreated between ?1 and ?2 and c.referrerRewarded = false and " +
             "c.registrationPlatform <> 'WEB' and c.referred in (select s.mintAccount from SavingsGoalEntity s where " +
             "s.savingsBalance >= ?3 and s.mintAccount = c.referred and " +
-            "s.creationSource = com.mintfintech.savingsms.domain.entities.enums.SavingsGoalCreationSourceConstant.CUSTOMER) order by c.dateCreated asc")
+            "s.savingsGoalType = com.mintfintech.savingsms.domain.entities.enums.SavingsGoalTypeConstant.CUSTOMER_SAVINGS) order by c.dateCreated asc")
     List<CustomerReferralEntity> getUnprocessedReferrals(LocalDateTime startDate, LocalDateTime endDate, BigDecimal savingsMinBalance, Pageable pageable);
 }
