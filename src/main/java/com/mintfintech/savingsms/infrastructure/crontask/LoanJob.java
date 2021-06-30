7f0e361a3a8b46ffe4d3ddeb64be7a5fe8d8819a
jnwanya <nwanyajustin@gmail.com>
2021-06-30 11:32:45 +0100

    update for staging

--- src/main/java/com/mintfintech/savingsms/infrastructure/crontask/LoanJob.java
+++ src/main/java/com/mintfintech/savingsms/infrastructure/crontask/LoanJob.java
@@ -42,8 +42,8 @@ public class LoanJob {
         } catch (Exception ignored) {
         }
     }
-//0 0 9/3 ? * *
-    @Scheduled(cron = "0 0/30 0 ? * *") // runs every 3 hour starting from 9am daily
+//0 0 9/3 ? * *  //0 0/30 0 ? * *
+    @Scheduled(cron = "0 0 11/3 ? * *") // runs every 3 hour starting from 9am daily
     @SchedulerLock(name = "LoanJob_processDueLoanPendingDebit", lockAtMostForString = "PT30M")
     public void processDueLoanPendingDebit() {
         loanRepaymentUseCase.checkDueLoanPendingDebit();
