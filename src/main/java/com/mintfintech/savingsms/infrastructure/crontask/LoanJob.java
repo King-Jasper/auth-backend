401b222ed9d8bd6d02a25ca428c6a6e823a88df9
jnwanya <nwanyajustin@gmail.com>
2021-07-01 21:40:39 +0100

    cleaned.

--- src/main/java/com/mintfintech/savingsms/infrastructure/crontask/LoanJob.java
+++ src/main/java/com/mintfintech/savingsms/infrastructure/crontask/LoanJob.java
@@ -27,11 +27,9 @@ public class LoanJob {
         loanRepaymentUseCase.dispatchEmailToCustomersWithPaymentDueInTwoDays();
     }
 
-    //   | 0 00 23 1/1 * ?
     @SchedulerLock(name = "LoanJob_loanRepaymentDueToday", lockAtMostForString = "PT45M")
-    @Scheduled(cron = "0 30 21 ? * *") // runs every day at 12:00pm.
+    @Scheduled(cron = "0 00 23 1/1 * ?") // runs every day at 12:00pm.
     public void loanRepaymentDueToday() {
-        log.info("loanRepaymentDueToday executes");
         loanRepaymentUseCase.loanRepaymentDueToday();
     }
 
