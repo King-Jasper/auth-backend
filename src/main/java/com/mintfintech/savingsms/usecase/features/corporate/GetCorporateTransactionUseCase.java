e4aa95533b6544e9f721ea9fc290f9bba47829ba
Olatunji124 <immamtunji@gmail.com>
2021-11-02 13:16:35 +0100

    investment liquidation request deails

--- src/main/java/com/mintfintech/savingsms/usecase/features/corporate/GetCorporateTransactionUseCase.java
+++ src/main/java/com/mintfintech/savingsms/usecase/features/corporate/GetCorporateTransactionUseCase.java
@@ -2,9 +2,11 @@ package com.mintfintech.savingsms.usecase.features.corporate;
 
 import com.mintfintech.savingsms.infrastructure.web.security.AuthenticatedUser;
 import com.mintfintech.savingsms.usecase.data.response.CorporateInvestmentDetailResponse;
+import com.mintfintech.savingsms.usecase.data.response.CorporateInvestmentLiquidationDetailResponse;
 import com.mintfintech.savingsms.usecase.data.response.CorporateInvestmentTopUpDetailResponse;
 
 public interface GetCorporateTransactionUseCase {
     CorporateInvestmentDetailResponse getInvestmentRequestDetail(AuthenticatedUser currentUser, String requestId);
     CorporateInvestmentTopUpDetailResponse getInvestmentTopUpRequestDetail(AuthenticatedUser currentUser, String requestId);
+    CorporateInvestmentLiquidationDetailResponse getInvestmentLiquidationRequestDetail(AuthenticatedUser currentUser, String requestId);
 }
