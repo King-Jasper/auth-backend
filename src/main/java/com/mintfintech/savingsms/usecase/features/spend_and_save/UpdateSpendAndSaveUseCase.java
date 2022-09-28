a2f7a712b33c2614748f77b753304a95a0ce730d
jnwanya <nwanyajustin@gmail.com>
2022-09-28 15:50:04 +0100

    updare

--- src/main/java/com/mintfintech/savingsms/usecase/features/spend_and_save/UpdateSpendAndSaveUseCase.java
+++ src/main/java/com/mintfintech/savingsms/usecase/features/spend_and_save/UpdateSpendAndSaveUseCase.java
@@ -2,7 +2,6 @@ package com.mintfintech.savingsms.usecase.features.spend_and_save;
 
 import com.mintfintech.savingsms.infrastructure.web.security.AuthenticatedUser;
 import com.mintfintech.savingsms.usecase.data.request.EditSpendAndSaveRequest;
-import com.mintfintech.savingsms.usecase.data.request.SpendAndSaveSetUpRequest;
 import com.mintfintech.savingsms.usecase.data.response.SpendAndSaveResponse;
 
 public interface UpdateSpendAndSaveUseCase {
