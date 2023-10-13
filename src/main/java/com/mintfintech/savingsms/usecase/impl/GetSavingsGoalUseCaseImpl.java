6960e42c448856a44617eb3a7057c6dff8252a84
jnwanya <nwanyajustin@gmail.com>
2023-10-13 08:28:48 +0100

    update for withholding tax

--- src/main/java/com/mintfintech/savingsms/usecase/impl/GetSavingsGoalUseCaseImpl.java
+++ src/main/java/com/mintfintech/savingsms/usecase/impl/GetSavingsGoalUseCaseImpl.java
@@ -79,12 +79,16 @@ public class GetSavingsGoalUseCaseImpl implements GetSavingsGoalUseCase {
         }
 
         BigDecimal interest = savingsGoalEntity.getAccruedInterest().setScale(2, RoundingMode.UP);
+        BigDecimal withHoldingTax = BigDecimal.ZERO;
+        if(savingsGoalEntity.getSavingsGoalType() == SavingsGoalTypeConstant.CUSTOMER_SAVINGS) {
+            withHoldingTax = interest.multiply(BigDecimal.valueOf(0.1));
+        }
         boolean isMatured = computeAvailableAmountUseCase.isMaturedSavingsGoal(savingsGoalEntity);
         SavingsGoalModel goalModel = new SavingsGoalModel();
         goalModel.setName(savingsGoalEntity.getName());
         goalModel.setGoalId(savingsGoalEntity.getGoalId());
         goalModel.setAccruedInterest(interest);
-        goalModel.setWithholdingTax(BigDecimal.ZERO);
+        goalModel.setWithholdingTax(withHoldingTax);
         goalModel.setTargetAmount(savingsGoalEntity.getTargetAmount());
         goalModel.setAvailableBalance(computeAvailableAmountUseCase.getAvailableAmount(savingsGoalEntity));
         goalModel.setAutoSaveEnabled(savingsGoalEntity.isAutoSave());
